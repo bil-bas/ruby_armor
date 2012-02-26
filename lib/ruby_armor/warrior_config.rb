0e302b50f2d85283b2cedd6e7faab9db0782c829
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2012-02-26 23:37:44 +0000

    Moved ruby armor config file into separate directory within the ruby warrior profile.

--- lib/ruby_armor/warrior_config.rb
+++ lib/ruby_armor/warrior_config.rb
@@ -1,12 +1,23 @@
+require 'fileutils'
+
 require_relative "base_user_data"
 
 module RubyArmor
   class WarriorConfig < BaseUserData
     DEFAULT_CONFIG = File.expand_path "../../../config/default_config.yml", __FILE__
-    CONFIG_FILE = "ruby_armour.yml"
+    OLD_CONFIG_FILE = "ruby_armour.yml"
+    CONFIG_FILE = "ruby_armor/config.yml"
 
     def initialize(profile)
-      super File.join(profile.player_path, CONFIG_FILE), DEFAULT_CONFIG
+      # Originally, config file was just in the folder. Move into its own folder, so we can put more in there.
+      old_config_file = File.join(profile.player_path, OLD_CONFIG_FILE)
+      config_file = File.join(profile.player_path, CONFIG_FILE)
+      if File.exists? old_config_file
+        FileUtils.mkdir_p File.dirname(config_file)
+        FileUtils.mv old_config_file, config_file
+      end
+
+      super config_file, DEFAULT_CONFIG
     end
 
     def turn_delay; data[:turn_delay]; end
